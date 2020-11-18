917f3f18c28124ee5e585205c8d16e2ead2812b9
rhetesi <rheetsi@gmail.com>
2020-11-18 16:39:16 +0100

    JS gyakorló feladatok 1-16

---
+++ js/main.js
@@ -0,0 +1,77 @@
+let array1 = [265, 123, 6334, 75543, 123, 2, 35, 23, 21, 897];
+
+/* 1. Írasd ki egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb legkisebb elemét! */
+
+/*
+let arrMin = arr[0];
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] < arrMin) {
+    arrMin = arr[i];
+  }
+}
+
+console.log(arrMin);
+*/
+
+function arrMin(arr) {
+    let min = arr[0];
+        for (let i = 1; i < arr.length; i += 1) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    } return min // Ha a funkciót hívom, ez lesz a visszatérési érték!!!
+}
+
+console.log(arrMin(array1));
+
+
+
+/* 2. Írasd ki egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb legnagyobb elemét! */
+
+function arrMax(arr) {
+    let max = arr[0];
+        for (let i = 1; i < arr.length; i += 1) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    } return max // Ha a funkciót hívom, ez lesz a visszatérési érték!!!
+}
+
+console.log(arrMax(array1));
+
+
+
+/* 3. Írasd ki egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb átlagát! */
+
+function arrAvg(arr) {
+    let sum = 0;
+    for (i = 0; i < arr.length; i += 1) {
+        sum = sum + arr[i];
+    }
+    let avg = sum / arr.length;
+    return avg;
+}
+
+console.log(arrAvg(array1));
+
+
+
+/* 4. Írasd ki egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb összegét! */
+
+function arrSum(arr) {
+    let sum = 0;
+    for (i = 0; i < arr.length; i += 1) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+console.log(arrSum(array1));
+
+
+
+/* 5. Írasd ki egy tetszőleges elemszámú, csak egész számokat tartalmazó tömb páros elemeinek a számát! */
+
+function arrEven1(arr) {
+    
+}
